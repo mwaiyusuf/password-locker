2daaf7cb1a77218ce5fa1f6ed204630cffebe9eb
mwaiyusuf <mwaigalo5@gmail.com>
2019-04-07 00:48:09 +0300

    initialisation of object

--- pass_test.py
+++ pass_test.py
@@ -1,10 +1,11 @@
 import unittest # Importing the unittest module
-from contact import Contact # Importing the contact class
+import pyperclip #importing a module
+from pass import Pass # Importing the pass class
 
-class TestContact(unittest.TestCase):
+class Testpass(unittest.TestCase):
 
     '''
-    Test class that defines test cases for the contact class behaviours.
+    Test class that defines test cases for the pass class behaviours.
 
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
@@ -13,15 +14,15 @@ class TestContact(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_contact = Contact("James","Muriuki","0712345678","james@ms.com") # create contact object
+        self.new_pass = Pass("James","Muriuki","0712345678","james@ms.com") # create pass object
  def test_init(self):
         '''
         test_init test case to test if the object is initialized properly
         '''
-         self.assertEqual(self.new_contact.first_name,"James")
-        self.assertEqual(self.new_contact.last_name,"Muriuki")
-        self.assertEqual(self.new_contact.phone_number,"0712345678")
-        self.assertEqual(self.new_contact.email,"james@ms.com")
+         self.assertEqual(self.new_pass.first_name,"James")
+        self.assertEqual(self.new_pass.last_name,"Muriuki")
+        self.assertEqual(self.new_pass.phone_number,"0712345678")
+        self.assertEqual(self.new_pass.email,"james@ms.com")
 
 
 if __name__ == '__main__':
