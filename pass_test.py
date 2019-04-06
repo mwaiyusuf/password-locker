3b44c839d52bf4601146439bd2e4f3c8872d9539
mwaiyusuf <mwaigalo5@gmail.com>
2019-04-06 23:59:39 +0300

    main

--- pass_test.py
+++ pass_test.py
@@ -18,3 +18,11 @@ class TestContact(unittest.TestCase):
         '''
         test_init test case to test if the object is initialized properly
         '''
+         self.assertEqual(self.new_contact.first_name,"James")
+        self.assertEqual(self.new_contact.last_name,"Muriuki")
+        self.assertEqual(self.new_contact.phone_number,"0712345678")
+        self.assertEqual(self.new_contact.email,"james@ms.com")
+
+
+if __name__ == '__main__':
+    unittest.main()
