30d9042fd08d3c3fbf54128757c7409738a44ea9
mwaiyusuf <mwaigalo5@gmail.com>
2019-04-07 01:53:14 +0300

    taaearDown

--- pass_test.py
+++ pass_test.py
@@ -25,8 +25,15 @@ class TestPass(unittest.TestCase):
         self.assertEqual(self.new_pass.email,"james@ms.com")
    def test_save_pass(self):
      '''
-     test_save_pass test case to test if the user object is saved into the pass list
+     test_save_pass test case to test if the user object is saved into the pass list 
      '''
+    self.new_pass.save_pass()  #saving the new user pass
+    self.assertEqual(len(Pass.pass_list),1)
 
+    def tearDown(self):
+      '''
+      tearDown method that does clean up after each test case has execute
+      '''
+      Pass.pass_list=[]
 if __name__ == '__main__':
     unittest.main()
