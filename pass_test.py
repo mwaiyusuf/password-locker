e54160ccbad85bb15ad21ff02d8c9a12d2a95683
mwaiyusuf <mwaigalo5@gmail.com>
2019-04-07 01:34:10 +0300

    the pass.py removed,,,i couldnt navigate int the file

--- pass_test.py
+++ pass_test.py
@@ -2,7 +2,7 @@ import unittest # Importing the unittest module
 import pyperclip #importing a module
 from pass import Pass # Importing the pass class
 
-class Testpass(unittest.TestCase):
+class TestPass(unittest.TestCase):
 
     '''
     Test class that defines test cases for the pass class behaviours.
@@ -23,7 +23,10 @@ class Testpass(unittest.TestCase):
         self.assertEqual(self.new_pass.user_name,"Muriuki")
         self.assertEqual(self.new_pass.password,"0712345678")
         self.assertEqual(self.new_pass.email,"james@ms.com")
-
+   def test_save_pass(self):
+     '''
+     test_save_pass test case to test if the user object is saved into the pass list
+     '''
 
 if __name__ == '__main__':
     unittest.main()
