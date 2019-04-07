af65a5f332b0667bf61dd2614b83860b46f66b13
mwaiyusuf <mwaigalo5@gmail.com>
2019-04-07 21:22:29 +0300

    finding a user and rerurns the user

---
+++ run.py
@@ -0,0 +1,21 @@
+from pass import passdef create_pass(flname,usname,password,email):
+'''
+creating a new user 
+'''
+new_pass = Pass(flname,usname,password,email)
+return new_pass
+ def save_users(pass):
+   '''
+   function to save pass
+   '''
+   user.save_pass()
+   def del_pass(user):
+     '''
+     function to delete a pass
+     '''
+     pass.delete_pass()
+     def find_pass(number):
+       '''
+       function that finds a usesr by number and rerurns into the use
+       '''
+       
