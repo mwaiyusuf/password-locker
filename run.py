1c2738d177af0fc5e64a6e956128a206d17e2668
mwaiyusuf <mwaigalo5@gmail.com>
2019-04-07 21:34:38 +0300

    checkin existing user

--- run.py
+++ run.py
@@ -18,3 +18,30 @@ return new_pass
        '''
        function that finds a usesr by number and rerurns into the use
        '''
+       return Pass.find_by_number(password)
+       def check_existing_users(number)
+       '''
+       function that check if a user exists with the number and return either true or false
+       '''
+       return Pass.user_exist(number)
+       def display_users():
+         '''
+         function that returns the saved users
+         '''
+         return user.display_users()
+         def main():
+           print("Hello, welcome. \n What is your name?")
+           user_name = input()
+           print(f"Hey {user_name}.What would you like to do?")
+           
+           while True:
+             list = (
+               '''
+               1.create new account
+               2.display the accounts
+               3.search for account
+               4.exist pass_locker
+               '''
+             )
+             print(list)
+             print("")
