5d83fa3872f80e4aacbf7a2c71b0dd20be46cd14
mwaiyusuf <mwaigalo5@gmail.com>
2019-04-07 20:32:46 +0300

    delete function

---
+++ pass.py
@@ -0,0 +1,41 @@
+import pyperclip
+
+
+class Pass:
+  '''
+  '''
+  pass_list = [] #an empty list
+   
+  def __init__(self,full_name,user_name,password,email):
+    
+    self.full_name = full_name
+    self.user_name = user_name
+    self.password = passwsord
+    sel.email = email
+
+    def save_pass(self):
+      '''
+      save_pass method saves user object inot pass_list
+      '''
+      Pass.pass_list.append(self)
+
+      def delete_pass(self):
+        '''
+        delete_pass method deletes a saved user from the pass_list
+        '''
+        Pass.pass_list.remove(self)
+        @classmethod
+        def find_by_number(cls,number):
+          '''
+          method that takes a number and returns a user that matches the number
+          
+          Args:
+          number : phone number to search for 
+          Returns : 
+          user that matches the number.
+          '''
+          for pass in cls.pass_list:
+            if pass.password == password:
+              rerurn pass
+              @classmethod
+              def pass_exist(cls,number);
